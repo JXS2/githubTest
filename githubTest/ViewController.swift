6b9803e82d0ccbc0ca7b53d82f300fcf7a0591ba
jaiden <jaiden@schraut.org>
2020-09-16 14:00:02 -0500

    Initial Commit

---
+++ githubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubTest
+//
+//  Created by Tiger Coder on 9/16/20.
+//  Copyright © 2020 clc.JXSchraut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
